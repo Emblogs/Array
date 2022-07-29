852255d8fc99d92ed5c8b272a6852d86ddf9231e
Emblogs <Emzy45cool@gmail.com>
2022-07-29 11:52:49 -0700

    modified

--- Activity.js
+++ Activity.js
@@ -1,11 +1,12 @@
-if(sunny){
+let weather="rainy"
+if(weather==="sunny"){
     console.log("I will go out with an umbrella and a sunglasses")
 }
 
-if(rainy){
+if(weather==="rainy"){
     console.log("I will go out with a coat, a glove and an umbrella")
 }
 
-if(clearsky){
-    console.log("go outand have fun")
+if(weather==="clearsky"){
+    console.log("go out and have fun")
 }
