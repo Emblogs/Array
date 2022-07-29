69ca57bac8c49ffb55f2daf441d8c2c117c17de0
Emblogs <Emzy45cool@gmail.com>
2022-07-29 11:45:41 -0700

    first commit

---
+++ Activity.js
@@ -0,0 +1,11 @@
+if(sunny){
+    console.log("I will go out with an umbrella and a sunglasses")
+}
+
+if(rainy){
+    console.log("I will go out with a coat, a glove and an umbrella")
+}
+
+if(clearsky){
+    console.log("go outand have fun")
+}
---
+++ index.js
@@ -0,0 +1,21 @@
+let array=[1,2,3]
+array[0]="one"
+
+array[2]="three"
+
+let pop=[1,2,3,4,5]
+
+pop[3]="wow, i did it"
+
+if(12){
+ console.log("Your eligible for codeville")}
+
+    if(90<50){
+    console.log("You fail")}
+
+console.log(true && false)
+console.log(true && true)
+console.log(false && false)
+console.log(false && true)
+console.log(array)
+console.log(pop)
